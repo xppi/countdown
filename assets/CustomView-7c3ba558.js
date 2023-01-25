1225935d3a805df92f94aca8aa8970c74250c840
kifuan <61240774+kifuan@users.noreply.github.com>
2023-01-25 07:49:37 +0000

    Deploying to gh-pages from @ kifuan/countdown@ad0b0caec50cdc90217a3e9d4f47ccbe1294af64 🚀

---
+++ assets/CustomView-7c3ba558.js
@@ -0,0 +1,1 @@
+import{d as a,c as r,k as c,o as m}from"./index-5c26761d.js";import{C as u}from"./Countdown-9c9a9ec0.js";const l=a({__name:"CustomView",setup(p){const o=c(),{year:t,month:e,day:n}=o.params,s=`${t}/${e}/${n}`;return(_,i)=>(m(),r(u,{until:s}))}});export{l as default};
